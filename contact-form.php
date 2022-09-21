91783b62897b9f050f98a91f6f19737b024b4831
Dhaval Gediya <dhavalg.magneto@gmail.com>
2022-09-21 23:29:31 +0530

    add library

--- contact-form.php
+++ contact-form.php
@@ -1,3 +1,52 @@
+<?php
+	include("mail/src/PHPMailer.php");
+	include("mail/src/Exception.php");
+	include("mail/src/SMTP.php");
+	
+	$mail = new PHPMailer\PHPMailer\PHPMailer();
+	$mail->IsSMTP();
+
+	$mail->Mailer = "smtp";
+	$mail->SMTPDebug  = 1;  
+$mail->SMTPAuth   = TRUE;
+$mail->SMTPSecure = "tls";
+$mail->Port       = 587;
+$mail->Host       = "smtp.gmail.com";
+$mail->Username   = "Richardyawboatengjr@gmail.com";
+$mail->Password   = "Elsa@12345";
+$mail->IsHTML(true);
+$mail->AddAddress("gediyadhaval0811@gmail.com", "recipient-name");
+$mail->SetFrom("Richardyawboatengjr@gmail.com", "from-name");
+$mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
+$content = "<b>This is a Test Email sent via Gmail SMTP Server using PHP mailer class.</b>";
+$mail->MsgHTML($content); 
+if(!$mail->Send()) {
+  echo "Error while sending Email.";
+  var_dump($mail);
+} else {
+  echo "Email sent successfully";
+}
+exit;
+	$email->SetFrom('noreply@pblcentral.co.uk', 'PBLCentral',0); //Name is optional
+	$email->Subject   = 'Action Note - ';
+	$email->Body      = "test";
+	$email->AddAddress('gediyadhaval0811@gmail.com');
+	//$email->AddCC('mosh680@gmail.com');
+
+	//$file_to_attach = $_SERVER["DOCUMENT_ROOT"].'/attachments/'.$filename;
+	//$email->AddAttachment( $file_to_attach );
+	// mail($to, $subject, $message, $headers);
+
+	if(!$email->Send())
+{
+   echo "Error sending: " . $email->ErrorInfo;
+}
+else
+{
+   echo "E-mail sent";
+}
+print_r($_POST);exit();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
