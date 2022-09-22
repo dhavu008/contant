5fecdfd5e5f3b34e1570e225af0f52a100281fab
Dhaval Gediya <dhavalg.magneto@gmail.com>
2022-09-22 14:34:39 +0530

    mail sent

--- contact-form.php
+++ contact-form.php
@@ -1,24 +1,58 @@
 <?php
-echo "dfg";
-echo php_info();exit;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 	include("mail/src/PHPMailer.php");
 	include("mail/src/Exception.php");
 	include("mail/src/SMTP.php");
 	
-	$mail = new PHPMailer\PHPMailer\PHPMailer();
+	$mail = new PHPMailer(true);
+
+	$name = $_POST['client_name'];
+	$client_email = $_POST['client_email'];
+	$subject = $_POST['subject'];
+	$client_message = $_POST['client_message'];
+	try {
+    // Server settings
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->SMTPSecure = false;
+    $mail->Port = 587;
+
+    $mail->Username = 'support@magnetoitsolutions.com'; // YOUR gmail email
+    $mail->Password = 'bB&G3849'; // YOUR gmail password
+
+    // Sender and recipient settings
+    $mail->setFrom('Richardyawboatengjr@gmail.com', 'Richard');
+    $mail->addAddress('gediyadhaval0811@gmail.com', 'Receiver Name');
+    $mail->addReplyTo('Richardyawboatengjr@gmail.com', 'Richard'); // to set the reply to
+    // Setting the email content
+    $mail->IsHTML(true);
+    $mail->Subject = "Contact Form";
+    $mail->Body = 'Name : '.$name.'<br>Client Email: '.$client_email.'<br> Subject : '.$subject.'<br> Message : '.$client_message.'.';
+
+    $mail->send();
+    header('location:index.html?success');
+} catch (Exception $e) {
+    echo "Error in sending email. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
+<!-- 
+	 $mail->SMTPDebug = SMTP::DEBUG_SERVER;
 	$mail->IsSMTP();
 
 	$mail->Mailer = "smtp";
-	$mail->SMTPDebug  = 1;  
 $mail->SMTPAuth   = TRUE;
 $mail->SMTPSecure = "tls";
 $mail->Port       = 587;
 $mail->Host       = "smtp.gmail.com";
-$mail->Username   = "Richardyawboatengjr@gmail.com";
+$mail->Username   = "support@magnetoitsolutions.com";
 $mail->Password   = "Elsa@12345";
 $mail->IsHTML(true);
 $mail->AddAddress("gediyadhaval0811@gmail.com", "recipient-name");
-$mail->SetFrom("Richardyawboatengjr@gmail.com", "from-name");
+$mail->SetFrom("support@magnetoitsolutions.com", "from-name");
 $mail->Subject = "Test is Test Email sent via Gmail SMTP Server using PHP Mailer";
 $content = "<b>This is a Test Email sent via Gmail SMTP Server using PHP mailer class.</b>";
 $mail->MsgHTML($content); 
@@ -28,4 +62,4 @@ if(!$mail->Send()) {
 } else {
   echo "Email sent successfully";
 }
-exit;
+exit; -->
